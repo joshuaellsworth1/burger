d193c750300017c5a2197da572bf1c5c33ff6fae
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-07 22:57:42 -0400

    Creating js and html files for setup

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require("express");
 
-var app = express();
 var PORT = process.env.PORT || 3000;
 
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
 });
