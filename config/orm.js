49bce684896e403d9663898b931f8d3a567fcca1
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-08 13:30:13 -0400

    created GET for router, now working on POST

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,46 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    all: function(tableInput, cb) {
-      var queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+
+    selectAll: function (id, name, cb) {
+        var burger = "INSERT INTO " + id;
+
+        burger += "(";
+        burger += name.toString();
+        burger += ") ";
+        burger += "VALUES (";
+        burger += ") ";
+        console.log(burger)
+
+        connection.query(burger, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+
+        })
+    },
+
+    insertOne: function () {
+
+    },
+
+    updateOne: function () {
+
     }
 
 
-    function selectAll(id, name, cb) {
-        var burger += " (";
-
-    }
-
-    function insertOne() {
-
-    }
-
-    function updateOne() {
-
-    }
-
+};
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,29 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var object = {
+            burger: data
+        };
+        console.log(object);
+        res.render("index", object);
+    });
+});
+
+router.post("/api/burger", function (req, res) {
+    burger.create([
+        "name"
+    ], [
+            req.body.name
+        ], function (result) {
+            res.json({ id: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var 
+})
 
 
 
