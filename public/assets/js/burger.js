4a9eea688cd9fbddf1b974cc9e75531ae269a92f
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-08 21:07:57 -0400

    worked in index.handlebars

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,10 +1,27 @@
 $(function() {
+    $(".devour-burger").on("click", function(event) {
+        var id = $(this).data("id");
+        var devouredBurger = $(this).data("devouredBurger");
+
+        var devouredBurgerState = {
+            devoured: devouredBurgerState
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredBurgerState
+        }).then(function() {
+            console.log("Burger Eaten", devouredBurger);
+            location.reload();
+        });
+    });
+    
     $(".create-form").on("submit", function(event) {
         event.preventDefault();
 
         var newBurger = {
-            burger_name: $("#newburger").val().trim(),
-            devoured: 0
+            name: $("#newburger").val().trim(),
+            burger: $("[name=burger]:checked").val().trim()
         };
 
         $.ajax("/api/burgers", {
@@ -16,21 +33,5 @@ $(function() {
         });
     });
 
-    $(".devour-burger").on("click", function(event) {
-        event.preventDefault();
-
-        var id = $(this).data("id");
-        var devouredBurger = {
-            devoured: 1
-        };
-
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: devouredBurger
-        }).then(function() {
-            console.log("Burger Eaten");
-            location.reload();
-        });
-    });
 
 })
